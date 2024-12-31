70ba17ce6d1038f29a40cc76cc56242cb17be5f9
Karixio <krzysztof678876@gmail.com>
2024-12-31 16:48:36 +0100

    added automatic detection of new chats and removed manulal check

--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 let availableChats = [];
 
 document.addEventListener('DOMContentLoaded', function() {
-  const buttonDetectChats = document.getElementById('detectBtn');
-  buttonDetectChats.addEventListener('click', onDetect);
+  onDetect();
   const buttonLeft = document.getElementById('btn');
   buttonLeft.addEventListener('click', goLeft);
 
@@ -17,14 +16,26 @@ const onDetect = async () => {
   chrome.scripting.executeScript({
     target: {tabId: tab.id},
     function: () => {
-      availableChats = [];
-      document.querySelectorAll('.x1ey2m1c.x78zum5.x164qtfw.xixxii4.x1vjfegm>div[data-visualcompletion="ignore"]').forEach((chatWindow, index) => {
-        chatWindow.setAttribute('data-chat-order', index);
-        availableChats.push(chatWindow);
-        console.log(availableChats);
-      });
+      const chatParent = document.querySelector('.x1ey2m1c.x78zum5.x164qtfw.xixxii4.x1vjfegm');
+      const chatParentConfig = { attributes: false, childList: true, subtree: false };
+
+      const callback = function(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+          if (mutation.type === 'childList') {
+            availableChats = [];
+            document.querySelectorAll('.x1ey2m1c.x78zum5.x164qtfw.xixxii4.x1vjfegm>div[data-visualcompletion="ignore"]').forEach((chatWindow, index) => {
+              chatWindow.setAttribute('data-chat-order', index);
+              availableChats.push(chatWindow);
+              console.log(availableChats);
+            });
+            chrome.runtime.sendMessage({message: 'changed'});
+          }
+        }
+      };
+      const observer = new MutationObserver(callback);
+      observer.observe(chatParent, chatParentConfig);
     }
-  })
+  });
   chrome.tabs.sendMessage(tab.id, {availableChats: availableChats});
 }
 
